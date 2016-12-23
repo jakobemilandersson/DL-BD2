d7a459c19c777c472bf3316fa2e4f652c847fb7a
Jakob <jakobemilandersson@gmail.com>
2016-12-24 00:40:44 +0100

    jaki

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var user_Control = require('./userControl');
 var room_Control = require('./roomControl.js');
 //Database:
 var mongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://heroku_kcplb6p5:helloQ07734@ds141328.mlab.com:41328/heroku_kcplb6p5';
+var url = 'mongodb://jaki:123@ds141368.mlab.com:41368/heroku_b774r87n';
 
 //Try to connect to MongoDB Database
 mongoClient.connect(url, function(err, db) {
