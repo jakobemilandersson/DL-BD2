37ea6f3bab74a7b4371798cae765bee6505a5325
jakobemilandersson <jakobemilandersson@github.com>
2016-12-25 19:09:17 +0100

    new ip...

--- userControl.js
+++ userControl.js
@@ -20,7 +20,8 @@ user_Control.userFunctions = function(data, socket, io){
 				//add user to list of users
 				addToUserList(data, socket);
 				//add userName+socketID+ip-address to MongoDB
-				mongoDB.collection('User').insert({'user':data.username, 'socketID':socket.id, 'ip':socket.handshake.address});
+				var ip = socket.handshake.headers ['x-forwarded-for'];
+				mongoDB.collection('User').insert({'user':data.username, 'socketID':socket.id, 'ip':ip});
 				//add user vote list of users
 				initUserVote(socket);
 				//send new userList to all clients
